bfb55ac605983110177909d3f73b7906addf49fa
Arthur Sandim de Oliveira <arthur.soliveira@inf.ufpel.edu.br>
2024-03-11 00:52:55 -0300

    IA base

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import tkinter as tk
 import emoji
+import copy
 
 class Piece(Enum):
     EMPTY = 0
@@ -16,6 +17,9 @@ class Breakthru:
 
         #TODO: Implementar a escolha aleatória do jogador inicial
         self.current_player = player #Define the current player
+        self.winner = None  # Define the winner
+        self.silver_pieces = 12 # Number of SILVER pieces
+        self.gold_pieces = 8 # Number of GOLD pieces (excluding the FLAGSHIP)
 
         # Config variables
         self.save_click = None # Save the first click to move the piece
@@ -132,7 +136,6 @@ class Breakthru:
             Piece.EMPTY: "E"
         }
 
-
         if self.save_click  is None:
             if piece_to_player[self.board[row][col]] != self.current_player and self.board[row][col] != Piece.EMPTY:
                 print_error("Não é a sua vez!")
@@ -151,11 +154,31 @@ class Breakthru:
             self.move_piece(self.save_click[0], self.save_click[1], row, col, bypass=True)
             self.save_click = None # Reseta o save_click
 
+            self.lock_ui()
+            self.ai_move()
+            self.unlock_ui()
+
+    def lock_ui(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.buttons[i][j].config(state="disabled")
+
+    def unlock_ui(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.buttons[i][j].config(state="normal")
+
     def move_piece(self, row, col, new_row, new_col, bypass=False):
 
         if not bypass and not self.is_move_valid(row, col, new_row, new_col):
             print_error("Movimento inválido!")
             return
+
+        if self.board[new_row][new_col] == (Piece.SILVER or Piece.GOLD):
+            if self.board[new_row][new_col] == Piece.SILVER:
+                self.silver_pieces -= 1
+            elif self.board[new_row][new_col] == Piece.GOLD:
+                self.gold_pieces -= 1
 
         # Move a peça para a nova posição
         self.board[new_row][new_col] = self.board[row][col]
@@ -168,6 +191,13 @@ class Breakthru:
         # Atualiza o jogador atual
         self.current_player = "G" if self.current_player == "S" else "S"
         self.window.title(f"{self.__class__.__name__} - {self.current_player} turn")
+
+
+        # Verifica se o jogo acabou
+        if self.is_game_over():
+            print("O jogo acabou!")
+            self.window.destroy()
+            print("Obrigado por jogar!")
 
 
     def is_move_valid(self, start_row, start_col, end_row, end_col):
@@ -198,21 +228,163 @@ class Breakthru:
             print_error("Célula inicial e final são as mesmas")
             return False  # Sem movimento
 
-        # Forca que o ataque seja na diagonal
-        if (self.board[start_row][start_col] != self.board[start_row][start_col] and abs(start_row - end_row) != abs(start_col - end_col)
-                or self.board[start_row][start_col]==Piece.EMPTY):
-            print_error("Ataque deve ser na diagona")
-            return False
+        #Movimento na horaizontal e vertical
+        if (start_row == end_row or start_col == end_col) and abs(start_row - end_row) <= 1 and abs(start_col - end_col) <= 1 and self.board[end_row][end_col] == Piece.EMPTY:
+            print("Movimento válido 1")
+            return True
 
+        #Ataque na diagonal
+        if abs(start_row - end_row) == abs(start_col - end_col) and piece_to_player[self.board[start_row][start_col]] != piece_to_player[self.board[end_row][end_col]] and self.board[end_row][end_col] != Piece.EMPTY:
+            print("Movimento válido 2")
+            return True
 
-        # Para movimentos diagonais, horizontais e verticais
-        if abs(start_row - end_row) <= 1 and abs(start_col - end_col) <= 1:
-            return True  # Movimento válido para uma célula adjacente
-
-
-        # Para a peça FLAGSHIP, você pode adicionar regras adicionais de movimento
-
+        print("Movimento inválido 3")
         return False  # Se nenhuma condição de movimento válido for atendida
+
+    def is_game_over(self):
+        # Encontra a posição do FLAGSHIP primeiro
+        flagship_position = None
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.board[i][j] == Piece.FLAGSHIP:
+                    flagship_position = (i, j)
+                    break  # Só há uma peça FLAGSHIP, então podemos parar quando encontrá-la
+            if flagship_position is not None:
+                break
+
+        # Se não encontramos a FLAGSHIP, ela foi capturada
+        if flagship_position is None:
+            print("O jogador SILVER venceu!")
+            self.winner = "S"
+            return True
+
+        # Verifica se o FLAGSHIP chegou à borda oposta
+        i, j = flagship_position
+        if i == 0 or i == self.rows - 1 or j == 0 or j == self.cols - 1:
+            print("O jogador GOLD venceu!")
+            self.winner = "G"
+            return True
+
+        #TODO: Não ta funcionando
+        # Verifica se o FLAGSHIP está cercado por peças SILVER nas diagonais
+        for di, dj in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
+            ni, nj = i + di, j + dj
+            # Verifica se o índice está dentro dos limites do tabuleiro
+            if 0 <= ni < self.rows and 0 <= nj < self.cols:
+                if self.board[ni][nj] != Piece.SILVER:
+                    # Se alguma diagonal não é SILVER, então o FLAGSHIP não está totalmente capturado
+                    return False
+
+        # Se todas as diagonais são SILVER, então o FLAGSHIP está capturado
+        print("O jogador SILVER venceu!")
+        return True
+
+    def get_valid_moves(self, i, j):
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        valid_moves = []
+
+        # Para simplificar, consideramos que todas as peças podem se mover em todas as direções.
+        # Você deve ajustar esta lógica com base nas regras de movimento das suas peças.
+        for di, dj in directions:
+            ni, nj = i + di, j + dj
+
+            # Verifica se o movimento está dentro dos limites do tabuleiro.
+            if 0 <= ni < self.rows and 0 <= nj < self.cols:
+                # Verifica se o movimento é válido de acordo com as regras do seu jogo.
+                # Por exemplo, a célula de destino está vazia ou contém uma peça adversária que pode ser capturada.
+                # Isso deve ser adaptado para as regras específicas do seu jogo.
+                if self.is_move_valid(i, j, ni, nj):
+                    valid_moves.append((ni, nj))
+
+        return valid_moves
+
+
+    def evaluate(self, player):
+        gold_witout_flagship = self.gold_pieces - 1
+        if player == "G":
+            return gold_witout_flagship - self.silver_pieces
+        else:
+            return self.silver_pieces - gold_witout_flagship
+
+
+    def minimax(self, depth, alpha, beta, maximizingPlayer):
+        if depth == 0 or self.is_game_over():
+            return self.evaluate(), None
+
+        if maximizingPlayer == "G":
+            maxEval = float('-inf')
+            best_move = None
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    if self.board[i][j] == Piece.GOLD:
+                        for k in range(self.rows):
+                            for l in range(self.cols):
+                                if self.is_move_valid(i, j, k, l):
+                                    self.move_piece(i, j, k, l)
+                                    eval, _ = self.minimax(depth - 1, alpha, beta, "S")
+                                    self.move_piece(k, l, i, j)
+                                    if eval > maxEval:
+                                        maxEval = eval
+                                        best_move = (i, j, k, l)
+                                    alpha = max(alpha, eval)
+                                    if beta <= alpha:
+                                        break
+            return maxEval, best_move
+
+    def minimax_with_alpha_beta(self, depth, alpha, beta, maximizing_player):
+        if depth == 0 or self.is_game_over():
+            return self.evaluate(), None
+
+        if maximizing_player == "G":
+            max_eval = float('-inf')
+            best_move = None
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    if self.board[i][j] == Piece.GOLD:
+                        piece_moves = self.get_valid_moves(i, j)
+                        for (k, l) in piece_moves:
+                            # Simulação do movimento sem alterar o tabuleiro principal
+                            new_board = copy.deepcopy(self.board)
+                            self.simulate_move(new_board, i, j, k, l)
+                            eval, _ = self.minimax_with_alpha_beta(depth - 1, alpha, beta, "S")
+                            # Desfazer movimento é desnecessário devido ao uso de deepcopy
+
+                            if eval > max_eval:
+                                max_eval = eval
+                                best_move = (i, j, k, l)
+                            alpha = max(alpha, eval)
+                            if beta <= alpha:
+                                break
+                if beta <= alpha:
+                    break
+            return max_eval, best_move
+        else:
+            min_eval = float('inf')
+            best_move = None
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    if self.board[i][j] == Piece.SILVER:
+                        piece_moves = self.get_valid_moves(i, j)
+                        for (k, l) in piece_moves:
+                            new_board = copy.deepcopy(self.board)
+                            self.simulate_move(new_board, i, j, k, l)
+                            eval, _ = self.minimax_with_alpha_beta(depth - 1, alpha, beta, "G")
+                            # Desfazer movimento é desnecessário devido ao uso de deepcopy
+
+                            if eval < min_eval:
+                                min_eval = eval
+                                best_move = (i, j, k, l)
+                            beta = min(beta, eval)
+                            if beta <= alpha:
+                                break
+                if beta <= alpha:
+                    break
+            return min_eval, best_move
+
+    def ai_move(self):
+        _, move = self.minimax_with_alpha_beta(3, float('-inf'), float('inf'), self.current_player)
+        if move:
+            self.move_piece(*move)
 
 class bcolors:
     ERROR = '\033[91m'
@@ -234,7 +406,7 @@ if __name__ == "__main__":
     print("*******************************************************************************")
     print("Escolha com qual time tu vais jogar: [S]ILVER ou [G]OLD?")
 
-    player = "G"
+    player = "S"
     while player not in ["S", "G"]:
         player = input().upper()
 
